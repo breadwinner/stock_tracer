915d36becd3437cac3c0a8f343d23724c71d6b86
breadwinner <li3yang3@gmail.com>
2026-01-13 08:30:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_data():
     # 建立连接
     conn = st.connection("gsheets", type=GSheetsConnection)
     # ttl=0 表示不缓存，每次都强制从云端拉取最新数据
-    df = conn.read(worksheet="Sheet1", ttl=60)
+    df = conn.read(worksheet="Sheet1", ttl=0)
     
     # 如果是空表，初始化列名
     if df.empty or len(df.columns) < len(COLUMNS):
